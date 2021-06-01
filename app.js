ed3e24e4a915e495845c784f82d35f6c41324349
oleshka275 <cat.dog.3415@gmail.com>
2021-06-01 19:30:55 +0300

    Js logic

--- app.js
+++ app.js
@@ -3,31 +3,36 @@ let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
 
-function draw(){
+function draw(userChoice, computerChoice){
     console.log("It's draw!");
+    result_p.innerHTML = userChoice + " don't beat " + computerChoice + ". It's draw!";
 }
 
 
-function win(){
+function win(userChoice, computerChoice){
     userScore++;
+    userScore_span.innerHTML = userScore;
     console.log("User Win!");
+    result_p.innerHTML = userChoice + " beat " + computerChoice + ". You win!";
 }
 
 
-function lose(){
+function lose(userChoice, computerChoice){
     computerScore++;
+    computerScore_span.innerHTML = computerScore;
     console.log("Computer Win!");
+    result_p.innerHTML = userChoice + " beat " + computerChoice + ". You lose!";
 }
 
 
 function getComputerChoice(){
-    const choices = ["r", "p", "s"];
+    const choices = ["rock", "paper", "scissors"];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
@@ -37,20 +42,20 @@ function checkMove(userChoice){
     const computerChoice = getComputerChoice();
 
     switch (userChoice+computerChoice){
-        case "rs":
-        case "pr":
-        case "sp":
-            win();
+        case "rockscissors":
+        case "paperrock":
+        case "scissorspaper":
+            win(userChoice, computerChoice);
             break;
-        case "rp":
-        case "ps":
-        case "sr":
-            lose();
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+            lose(userChoice, computerChoice);
             break;
-        case "rr":
-        case "pp":
-        case "ss":
-            draw();
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            draw(userChoice, computerChoice);
             break;
     }
 }
@@ -59,15 +64,15 @@ function checkMove(userChoice){
 function main(){
 
     rock_div.addEventListener('click', function(){
-        checkMove("r");
+        checkMove("rock");
     })
     
     paper_div.addEventListener('click', function(){
-        checkMove("p");
+        checkMove("paper");
     })
     
     scissors_div.addEventListener('click', function(){
-        checkMove("s");
+        checkMove("scissors");
     })
 }
 
